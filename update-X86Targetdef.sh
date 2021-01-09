2b37a11c0efe400ece2ef6860bb29b17c2c600ef
Matt Stancliff <matt@genges.com>
2021-01-09 16:14:25 -0700

    Note llvm stopped updating our metadata feed
    We'll need to refactor generateCPUDetailsToFeatures.py to support the
    new in-llvm CPU feature map definition files since they stopped updating
    the old format in mid-2020.

--- update-X86Targetdef.sh
+++ update-X86Targetdef.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
+# Old:
 curl -O https://raw.githubusercontent.com/llvm-mirror/clang/master/include/clang/Basic/X86Target.def
+
+# Note: as of this commit: https://github.com/llvm/llvm-project/commit/d5c28c4094324e94f6eee403022ca21c8d76998e
+# llvm moved the nice feature flags from X86Target into X86Target and X86TargetParser.
+
+# If we want to support ongoing llvm development past cooperlake, we need to refactor
+# the definition parser to use the new file formats (which looks like it'll require
+# maintaining more state details since we have to sync both files now?).
+
+# For now we're just using the old format (which won't be updated anymore), but
+# new CPUs aren't being added at such a huge rate of advancement where it
+# matters at the moment.
+
+# New:
+#curl -O https://raw.githubusercontent.com/llvm/llvm-project/main/clang/include/clang/Basic/X86Target.def
+#curl -O https://raw.githubusercontent.com/llvm/llvm-project/main/llvm/include/llvm/Support/X86TargetParser.def
